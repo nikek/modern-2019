94d72e5b6510b4c6880de2578ec23825489ea45c
nikek <nikek@spotify.com>
2019-07-03 17:06:38 +0200

    add favicon and put body styles in <head>

--- src/core/App.js
+++ src/core/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import Nav from '/core/Nav';
 import HomePage from '/plugins/home/HomePage';
 import AboutPage from '/plugins/about/AboutPage';
-
-const styles = {
-  height: '100vh',
-  width: '100vw',
-  backgroundColor: '#333',
-  fontFamily: 'sans-serif',
-  color: '#ddd',
-};
-
 import { useRoutes } from 'hookrouter';
 
 const routes = {
@@ -22,10 +13,10 @@ const Router = () => useRoutes(routes) || 'nothing here';
 
 export default function App() {
   return (
-    <div style={styles}>
+    <>
       <h1>App is alive</h1>
       <Nav />
       <Router />
-    </div>
+    </>
   );
 }
