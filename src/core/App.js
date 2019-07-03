8c02a137958c5b66abba85c3d305124d2b905398
nikek <nikek@spotify.com>
2019-07-03 02:40:35 +0200

    add React and make it work

---
+++ src/core/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const styles = {
+  height: "100vh",
+  width: "100vw",
+  backgroundColor: "#333",
+  fontFamily: "sans-serif",
+  color: "#ddd"
+};
+
+export default function App() {
+  return <div style={styles}>App is alive</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hej");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "/core/App";
+
+ReactDOM.render(<App />, document.getElementById("app-root"));
