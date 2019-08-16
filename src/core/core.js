cac265192cf57bfecf87027bf3176fdb600b6b7f
nikek <nikek@spotify.com>
2019-08-16 16:50:13 +0200

    add apollo and a test query

--- src/core/core.js
+++ src/core/core.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import App from '/core/app/components/App';
 import * as plugins from '/plugins/pluginRegister';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import config from '../config.json';
+
+const client = new ApolloClient({
+  uri: config.grapqhlUrl,
+});
+
 Object.values(plugins).forEach(p => p.init());
 
-ReactDOM.render(<App />, document.getElementById('app-root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app-root')
+);
--- src/plugins/service-overview/ServiceOverviewPage.js
+++ src/plugins/service-overview/ServiceOverviewPage.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
 export default function ServiceOverviewPage({ id }) {
+  const { loading, error, data } = useQuery(gql`
+    query {
+      user(username: "${id}") {
+        components {
+          id
+        }
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <article>
+      Hejsan
+      <ul>
+        {data.user.components.map(c => (
+          <li>{c.id}</li>
+        ))}
+      </ul>
+    </article>
+  );
   return <div>Service Overview for {id}</div>;
 }
